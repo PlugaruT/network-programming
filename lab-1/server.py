eac0b29871eea87e7b5c94aca7d0d628309e732a
keatinge <willy.keatinge@gmail.com>
2016-04-28 16:45:29 -0400

    fixed 42 case to ensure that string ENDS with a ?, not just contains one

--- lab-1/server.py
+++ lab-1/server.py
@@ -32,7 +32,8 @@ def client_thread(connection):
         elif command == 'div':
             numbers = tokens[1].split('/')
             reply = str(int(numbers[0]) / int(numbers[1]))
-        elif command.find('?') > 0:
+        elif command[-1:] == "?":
+			#makes sure the command ends with a '?', not just contains one
             reply = '42'
         elif command == 'rev':
             reply = tokens[1][::-1]
